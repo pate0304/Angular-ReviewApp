4ae07bb9d66e34bc4fa822a38136c6eb99648b3d
Jay Patel <pate0304@algonquinlive.com>
2016-10-16 23:21:06 -0400

    udated

--- js/main.js
+++ js/main.js
@@ -2,11 +2,6 @@ var translationsEN = {
     username: 'User Name',
     writereview: 'Write Review',
     submit: 'Submit',
-    PARAGRAPH: 'Srsly!',
-    PASSED_AS_TEXT: 'Hey there! I\'m passed as text value!',
-    PASSED_AS_ATTRIBUTE: 'I\'m passed as attribute value, cool ha?',
-    PASSED_AS_INTERPOLATION: 'Beginners! I\'m interpolated!',
-    VARIABLE_REPLACEMENT: 'Hi {{name}}',
     BUTTON_LANG_FR: 'French',
     BUTTON_LANG_EN: 'English'
 };
@@ -15,23 +10,18 @@ var translationsFR = {
     username: 'Nom dutilisateur',
     writereview: 'Ecrire une critique',
     submit: 'soumettre',
-    HEADLINE: 'Was für ein großartiges Modul!',
-    PARAGRAPH: 'Ernsthaft!',
-    PASSED_AS_TEXT: 'Hey! Ich wurde als text übergeben!',
-    PASSED_AS_ATTRIBUTE: 'Ich wurde als Attribut übergeben, cool oder?',
-    PASSED_AS_INTERPOLATION: 'Anfänger! Ich bin interpoliert!',
-    VARIABLE_REPLACEMENT: 'Hi {{name}}',
     BUTTON_LANG_FR: 'Français',
     BUTTON_LANG_EN: 'Anglais'
 };
 
-var app = angular.module('myApp', ['pascalprecht.translate', 'ui.bootstrap']);
+var app = angular.module('myApp', ['pascalprecht.translate', 'ui.bootstrap', 'ngCookies']);
 
 app.config(['$translateProvider', function ($translateProvider) {
     // add translation tables
     $translateProvider.translations('en', translationsEN);
     $translateProvider.translations('fr', translationsFR);
-    $translateProvider.preferredLanguage('en');
+    // $translateProvider.preferredLanguage('en');
+    $translateProvider.useCookieStorage();
 
 }]);
 
@@ -43,19 +33,12 @@ app.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
     $scope.pageSize = 2;
     $scope.currentPage = 1;
     $scope.reviews = [];
-//    localStorage.setItem("Pref", langKey);
     for (item in localStorage) {
 
-        var newItem = localStorage[item];
+        var newItem = JSON.parse(JSON.stringify(localStorage[item]));
         $scope.reviews.push(newItem);
     }
 
-    //    for (item in localStorage) {
-    //
-    //        var newItem = JSON.parse(localStorage[item]);
-    //        $scope.reviews.push(newItem);
-    //    }
-    //    console.log("Here");
     $scope.submit = function (uname, review) {
 
         $scope.show = true;
@@ -73,7 +56,8 @@ app.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
     }
 
 
-            }]);
+}]);
+
 app.filter('start', function () {
     return function (input, start) {
         if (!input || !input.length) {
